6a9c6b492270fbf896a985f1b3374127d001fb5b
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-02-19 18:02:50 -0600

    added paragraoh

--- js/Home.js
+++ js/Home.js
@@ -7,10 +7,10 @@ export default React.createClass({
         <section>
           <article className="home-Bio-Wrapper">
             <h2> Bio Snippet </h2>
-            <p className="home-bio"> Hi everyone, welcome to my newly created portfolio. Just wanted to say a little bit about myself. My family and I moved to San Antonio June of 2015 and its been a crazy whirlwind of the unexpected. Lets just say Murphy paid a visit from time to time. ;)
+            <p className="home-bio"> Hi everyone, welcome to my portfolio. Just wanted to say a little bit about myself. My family and I moved to San Antonio June of 2015 and its been a crazy whirlwind of the unexpected. Lets just say Murphy paid a visit from time to time. ;)
               (If you didn't get that joke then click <a href="https://en.wikipedia.org/wiki/Murphy's_law" target="_blank"> here.</a>)
             </p>
-            <p className="home-bio"> Anyways, I have a formal degree from the Texas A&M University-Kingsville in music education. Since moving to San Antonio however it has been extremly difficult finding a job. So what did I do? I changed careers. Now im a student at <a href="https://www.theironyard.com/" target="_blank"> The Iron Yard </a> studying to be a Front-End Software Developer. This site is my account of my journey and experience learning how to code, as well as some projects that I've done. Hope you enjoy.</p>
+            <p className="home-bio"> Anyways, I have a formal degree from the Texas A&M University-Kingsville in music education, Since moving to San Antonio I decided to change careers and pursue software development. Learning how to code has been a great experience for me. I have been able to meet some great people and have developed a passion for my new found skill. My goal is to keep on learning and growing. Im ready to take on new challenges and be successful. This site is my account of my journey and experience. Hope you enjoy.</p>
           </article>
        </section>
     )
