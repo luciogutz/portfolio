c0b0fe3debcda0fbba19b6db415879f6f1f1db2f
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-03-02 13:13:51 -0600

    added axios for api call

--- js/Home.js
+++ js/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
+import { ajax } from 'jquery'
+import axios from 'axios'
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      articles: {
+          id: "",
+          name: "",
+          description: "",
+          url: "",
+          category: ""
+      }
+    }
+  },
+  componentDidMount: function() {
+    axios.get("https://newsapi.org/v1/articles?source=techcrunch&sortBy=latest&apiKey=1a07ab283b5e43b898acbf04efbb3136").then(function(results){
+      console.log(results);
+    })
+  },
   render() {
       return (
         <section>
