3d02ff1624bb58ba77cf18c8081ab3ce8d7f0358
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-03-07 13:34:08 -0600

    changed text of development paragraph

--- js/Home.js
+++ js/Home.js
@@ -30,7 +30,7 @@ export default React.createClass({
           <section className="developing__Banner">
             <article className="developing">
               <h1 className="developing__page--Title"> Developing </h1>
-              <p className="developing__Text"> My skill set is based on front end development, and I'm continuing to work on those aspects of my abilities. Currently however I have begun to learn PHP and that has been fun to learn. </p>
+              <p className="developing__Text"> My skill set is based on front end development, and I'm continuing to work on those aspects of my abilities. Currently however I have begun to learn PHP and that has been a steady process. </p>
               <p className="developing__Text"> Future goals are to get into mobile applications. Perhaps React Native is a route I will consider taking, or straight IOS or Android development. Either way it ought to be very interesting. </p>
             </article>
             <h2 className="developing__page--Contact"> Contact </h2>
