9869ed7371e243bc04192715e36cd54b4f4bcef1
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-03-07 13:08:57 -0600

    will deploy on heroku

--- js/Header.js
+++ js/Header.js
@@ -15,7 +15,6 @@ export default React.createClass ({
         <p className="welcome"> Hi, my name is Lucio Gutierrez and I am a Front end Developer from San Antonio, TX. Just graduated from The Iron Yard and am excited to see where this new found passion will take me.  </p>
         <section className="menu">
           <Link to="/" className="homeButton hvr-grow">Home</Link>
-          <a onClick={this.onProjectsClick} href="#" className="projectsButton hvr-grow">Projects</a>
           <Link href="https://medium.com/@lucius956" target="_blank" className="blogButton hvr-grow">Blog</Link>
           <Link to="/Resume" className="resumeButton hvr-grow">Resume</Link>
           <a href="https://github.com/luciogutz?tab=repositories" target="_blank" className="gitHubButton hvr-grow">GitHub</a>
